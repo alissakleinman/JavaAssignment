a36cce0aa56dc5c3fdcfdd734bed0ecfa4c22f46
Alissa Kleinman <alissa.kleinman@gmail.com>
2014-12-03 20:34:46 -0500

    changes to .js

--- assignment.js
+++ assignment.js
@@ -1,1 +1,33 @@
-var number = 3
+//These are examples of variables.
+	
+//Some variables are integers.
+
+var x = 4;
+var y = 7;
+
+//Some variables are strings
+var statement = "Cheese is my favorite food.";
+var z= "Dairy";
+var sentence = "One of my favorite cheeses is Gruyere.";
+
+//This is an array.
+var cheeses = ["Gruyere, Manchego, Swiss, Labneh, Camembert"];
+var numbers = [2, 3, 4, 5, 6]
+
+//This is an example of a multi-dimensional array.
+
+var crazycombo = [cheeses, numbers];
+
+//Here's a test of some variables with an alert.
+
+alert(x + y);
+alert(crazycombo)
+
+//Here's another test using the console.
+console.log(cheeses)
+console.log(x + y)
+
+
+alert(sentence);
+
+//I can also test using the console log.
