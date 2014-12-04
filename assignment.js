6c01f2df497bb7ba569420292bfbc40926a1cac3
Alissa Kleinman <alissa.kleinman@gmail.com>
2014-12-03 22:01:46 -0500

    more examples

--- assignment.js
+++ assignment.js
@@ -10,24 +10,65 @@ var statement = "Cheese is my favorite food.";
 var z= "Dairy";
 var sentence = "One of my favorite cheeses is Gruyere.";
 
+//This is an example of a Boolean data type.
+var TheKnicksAreBadThisYear = true;
+
 //This is an array.
-var cheeses = ["Gruyere, Manchego, Swiss, Labneh, Camembert"];
+var cheeses = ["Gruyere", "Manchego", "Swiss", "Labneh", "Camembert"];
 var numbers = [2, 3, 4, 5, 6]
 
 //This is an example of a multi-dimensional array.
 
 var crazycombo = [cheeses, numbers];
 
+//I think this is an example of arrayception. 
+
+console.log(crazycombo[0][0]);
+//The above command should access the first item in the first array, which is Gruyere. It does! Hooray.
+
+//The above comment could have been multi-line, in which it would have looked like:
+
+/*This
+is an example
+of a comment that stretches
+across multiple lines*/
+
 //Here's a test of some variables with an alert.
 
 alert(x + y);
-alert(crazycombo)
-
-//Here's another test using the console.
-console.log(cheeses)
-console.log(x + y)
-
-
+alert(crazycombo);
 alert(sentence);
 
-//I can also test using the console log.
+//Here's another test using the console.
+console.log(cheeses);
+console.log(x + y);
+
+//Another way to test is to log directly to html. I'm going to show this directly on assignment.hmtl.
+
+//Here is a logic test:
+
+if (x > y){
+	console.log("you won't see this because 4 is less than 7");
+} else{
+	console.log("you'll see this message")
+}
+
+//Here are some functions, all tested in the console:
+function addThree (number){
+	return number + 3;
+}
+console.log(addThree(4));
+
+function alertCheese (cheesename){
+	return alert(cheesename);
+}
+alertCheese("Manchego");
+
+function multiply (a, b){
+	return a * b;
+}
+console.log(multiply (4, 3));
+	
+
+
+
